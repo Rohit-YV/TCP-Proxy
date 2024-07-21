e14dee003f93035cb214799fe7f0f87c4ffcf08e
Ranjan-Yadav <111560232+Rohit-YV@users.noreply.github.com>
2024-07-21 19:42:16 +0530

    Update TCP_proxyV1.py

--- TCP_proxyV1.py
+++ TCP_proxyV1.py
@@ -19,17 +19,34 @@ def hexdump(src, length=16, show=True):
             print(line)
     else:
         return result
-    if isinstance(src,bytes):
-        src =src.decode()
-    result = list()
-    for i  in range (0,len(src),length):
-        word = str(src[i:i+length])
-        printable = word.translate(HEX_FILTER)
-        hexa = ' '.join([f'{ord(c):202X}' for c in word])
-        hexwidth = length * 3
-        results.append(f'{i:04x}) {hexa:<{hexwidth}} {printable}')
-    if show:
-        for line in results:
-            print(line)
-    else:
-        return results
+
+def receive_form(connection):
+    buffer = b""
+    connection.settimeout(5)
+    try:
+        while True:
+            data = connection.recv(4096)
+            if not data:
+                break
+            buffer += data
+    except Exception as e:
+        pass
+    return buffer
+
+def request_handler(buffer):
+    return buffer
+
+def response_handler(buffer):
+    return buffer
+
+# def proxy_handler(client_socket,remote_host,remote_port,receive_first):
+#     remote_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#     remote_socket.connect((remote_host, remote_port))
+#     if receive_first:
+#         remote_buffer = receive_form(remote_socket)
+#         hexdump(remote_buffer)
+#     remote_buffer =response_handler(remote_buffer)
+#     if len(remote_buffer):
+#         print("[<==] Sending %d bytes to localhost ." %len(remote_buffer))
+#         client_socket.send(remote_buffer)
+    
